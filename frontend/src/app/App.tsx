bf19903afc8cdbac356671d47472a1a2cc0403a6
OmRanALot <omranamile@gmail.com>
2026-03-01 01:47:33 -0500

    got rid of frontend folder

--- frontend/src/app/App.tsx
+++
@@ -1,9 +0,0 @@
-import { AgentConsole } from './components/AgentConsole';
-
-export default function App() {
-  return (
-    <div className="size-full flex items-center justify-center bg-gradient-to-br from-blue-50 via-white to-blue-100 p-8">
-      <AgentConsole />
-    </div>
-  );
-}
